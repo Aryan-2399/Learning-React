087c4d101fed8af244dfae847d5f03d31637b600
Aryan <23.aryan99@gmail.com>
2026-03-01 12:18:55 +0530

    EPISODE-03

--- App.js
+++ App.js
@@ -1,66 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-// const heading = React.createElement("h1", {
-//     id :'heading'
-// }, "Hello from react");
-const heading = React.createElement(
-  "div",
-  {
-    id: "parent",
-  },
-  [
-    React.createElement(
-      "div",
-      {
-        id: "child 1",
-        key: "c1h1"
-      },
-      [
-        React.createElement(
-          "h1",
-          {
-            id: "heading1",
-            key:"c1h2"
-          },
-          "Heading1"
-        ),
-        React.createElement(
-          "h1",
-          {
-            id: "heading2",
-            key: "c1h3"
-          },
-          "Heading1"
-        ),
-      ]
-    ),
-    React.createElement(
-      "div",
-      {
-        id: "child 2",
-        key:"c2h1"
-      },
-      [
-        React.createElement(
-          "h1",
-          {
-            id: "heading3",
-            key: "c2h2"
-          },
-          "Heading1"
-        ),
-        React.createElement(
-          "h1",
-          {
-            id: "heading4",
-            key: "c2h3"
-          },
-          "Heading1"
-        ),
-      ]
-    ),
-  ]
+import React from "react";
+import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+const Title = function () {
+  return <h1>Namaste React using JSXs</h1>;
+};
+
+//JSX- HTML like Syntax
+// JSX Heading ===
+const jsxHeading = <h1>This is a JSX Heading</h1>;
+
+//React Functional Components=>  JS Function which returns a JSX
+const FunctionComponent = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h1>React Functional Component Heading</h1>
+    </div>
+  );
+};
+
+//render js inside JSX---- put it inside {}
+const num = 100;
+const jsInsidezJSX = (
+  <div>
+    <span>{num * 3}</span>
+  </div>
 );
+
+
+//Similar way you can render React Element inside JSX as React Element is basically JS Objects
+const reactEl = React.createElement('h1', {id: "reactEl"}, "React element Heading");
+const reactElementInsidezJSX = (
+  <div>
+    <span>{reactEl}</span>
+  </div>
+);
+
+
 const root = createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<FunctionComponent />);
